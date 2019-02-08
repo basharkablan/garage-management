39bb9cf97fbf91c131b7be23bfae20ef53e18e4f
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-02-08 23:52:22 +0200

    changed jquery lib, login page posts server, need server auth

--- routes/auth-post.js
+++ routes/auth-post.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var messages = require('../messages');
+
 router.post('/login',(req, res) => {
-    res.send(JSON.stringify({result : 'OK'}));
+    username = req.body.username;
+    password = req.body.password;
+
+    console.log(username + " - " + password);
+
+    res.send(JSON.stringify(messages.wrong_cred));
 });
 
 module.exports = router;
