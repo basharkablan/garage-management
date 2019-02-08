736ad71bb21188d7b53e01f2d4b38bbd1574a946
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-02-09 00:21:12 +0200

    added favicon

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+const favicon = require('express-favicon');
+
 global.__basedir = __dirname;
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
