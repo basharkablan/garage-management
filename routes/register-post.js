23caf925e4bfacaa488877aba7aea4c182de2b84
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-02-11 20:45:17 +0200

    added register post resopnse handler, need server request handler

--- routes/register-post.js
+++ routes/register-post.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var messages = require('../messages');
+
 router.post('/register', (req, res) => {
     //res.sendFile(__basedir + '/html/register.html');
-    res.send(JSON.stringify({result: 'OK'}));
+    res.send(JSON.stringify(messages.success));
 });
 
 module.exports = router;
