26187be3e0112d069e694b2842d7fd5ab868456f
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-02-14 01:21:37 +0200

    login form fully working, need to send sessions

--- routes/auth-post.js
+++ routes/auth-post.js
@@ -2,14 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 var messages = require('../messages');
+var models = require("../db/models");
 
-router.post('/login',(req, res) => {
+router.post('/login', function (req, res) {
     username = req.body.username;
     password = req.body.password;
 
-    console.log(username + " - " + password);
+    if(username == undefined || password == undefined || username.trim() == "" || password.trim() == "") {
+        res.send(JSON.stringify(messages.missing_field));
+        return;
+    }
 
-    res.send(JSON.stringify(messages.success));
+    models.user.findOne({username : username, password : password}, (err, result) => {
+        if(err) {
+            console.log(err);
+            res.send(JSON.stringify(messages.server_internal_error));
+            return;
+        }
+        if(result == undefined) {
+            res.send(JSON.stringify(messages.wrong_cred));
+            return;
+        }
+        console.log("login: " + username + " - " + password);
+        res.send(JSON.stringify(messages.success));
+    });
 });
 
 module.exports = router;
