2670705bac719158afb37755440d5264e1da6779
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-02-18 12:56:13 +0200

    removed unused functionality, updated TODO

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,3 @@
-function show1() {
-    $("#page1").show();
-    $("#page2").hide();
-    $("#page3").hide();
-    $("#page4").hide();
-}
-function show2() {
-    $("#page1").hide();
-    $("#page2").show();
-    $("#page3").hide();
-    $("#page4").hide();
-}
-function show3() {
-    $("#page1").hide();
-    $("#page2").hide();
-    $("#page3").show();
-    $("#page4").hide();
-}
-function show4() {
-    $("#page1").hide();
-    $("#page2").hide();
-    $("#page3").hide();
-    $("#page4").show();
-}
-
 function nav_btn_hover_in() {
     $("#overlay").addClass("overlay");
 }
@@ -31,24 +6,14 @@ function nav_btn_hover_out() {
     $("#overlay").removeClass("overlay");
 }
 
-$(document).ready(function() {
-    show1();
-    
-    $("#nav_btn_1").hover(nav_btn_hover_in, nav_btn_hover_out);
-    $("#nav_btn_2").hover(nav_btn_hover_in, nav_btn_hover_out);
-    $("#nav_btn_3").hover(nav_btn_hover_in, nav_btn_hover_out);
-    $("#nav_btn_4").hover(nav_btn_hover_in, nav_btn_hover_out);
-});
-
 function searchFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("searchInput");
     filter = input.value.toUpperCase();
-    //cards = $("#accordionExample").find(".card");
     accordion = document.getElementById("accordionExample");
     cards = accordion.getElementsByClassName("card");
     for (i = 0; i < cards.length; i++) {
-        
+
         carNumber = cards[i].getElementsByClassName("carNumberBody")[0];
         carNumberSearch = carNumber.textContent || carNumber.innerText;
 
--- public/js/maintenance-ang.js
+++ public/js/maintenance-ang.js
@@ -1,7 +1,7 @@
-// Define the `phonecatApp` module
+// Define the `maintenanceApp` module
 const maintenanceApp = angular.module('maintenance-ang', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
+// Define the `CarRecordsController` controller on the `maintenanceApp` module
 maintenanceApp.controller('CarRecordsController', function CarRecordsController($scope, $http) {
     $http.post("get-maintenance-list").then(function(response) {
         $scope.carRecords = response.data.result;
