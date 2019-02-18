53b690c0e419b5747606bc010a338c872dbe191f
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-02-18 12:49:23 +0200

    extended search to carNumber, brand, model and year

--- public/js/index.js
+++ public/js/index.js
@@ -48,9 +48,26 @@ function searchFunction() {
     accordion = document.getElementById("accordionExample");
     cards = accordion.getElementsByClassName("card");
     for (i = 0; i < cards.length; i++) {
-        carNumber = cards[i].getElementsByClassName("carNumberTitle")[0];
-        txtValue = carNumber.textContent || carNumber.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        
+        carNumber = cards[i].getElementsByClassName("carNumberBody")[0];
+        carNumberSearch = carNumber.textContent || carNumber.innerText;
+
+        brand = cards[i].getElementsByClassName("brandNameBody")[0];
+        brandSearch = brand.textContent || brand.innerText;
+
+        model = cards[i].getElementsByClassName("modelBody")[0];
+        modelSearch = model.textContent || model.innerText;
+
+        model = cards[i].getElementsByClassName("modelBody")[0];
+        modelSearch = model.textContent || model.innerText;
+
+        year = cards[i].getElementsByClassName("yearBody")[0];
+        yearSearch = year.textContent || year.innerText;
+
+        if (carNumberSearch.toUpperCase().indexOf(filter.trim()) > -1 ||
+            brandSearch.toUpperCase().indexOf(filter.trim()) > -1 ||
+            modelSearch.toUpperCase().indexOf(filter.trim()) > -1 ||
+            yearSearch.toUpperCase().indexOf(filter.trim()) > -1) {
             cards[i].style.display = "";
         } else {
             cards[i].style.display = "none";
